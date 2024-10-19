792789b7aa7bf0e78229e39747cf8e59fe3a24af
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2024-10-19 18:51:14 -0300

    Instalação do Laravel, React e criação do DB

---
+++ backend/database/migrations/2024_10_19_213721_create_meeting_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('meeting', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('room_id');
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->dateTime('start_at');
+            $table->dateTime('end_at');
+            $table->timestamps();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meeting');
+    }
+};
---
+++ readme.md
@@ -0,0 +1,12 @@
+<></>
+<h1>Desafio NetWall</h1>
+<h2>Introdução</h2>
+<p>Este é um desafio criado pela empresa Net Wall para o cargo de desenvolvedor full stack. Nele foram utilizadas as tecnologias Laravel e React no back en e front end respectivamente.</p>
+
+<h2>Instalação e Configuração</h2>
+
+<h2>Front End</h2>
+
+<h2>Back End</h2>
+
+<h2>Banco de Dados</h2>
